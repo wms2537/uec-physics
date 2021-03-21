5cf8d775494d145824662f091569f8bcb41e5e6a
wms2537 <swmeng@yes.my>
2021-03-21 15:31:41 +0800

    fix connection

--- generate_pdfs.sh
+++ generate_pdfs.sh
@@ -6,7 +6,7 @@ sleep 30
 docker logs drawio
 for filename in ./diagrams/*.drawio; do
     echo "### rendering $filename ..."
-        curl -d @./diagrams/$(basename $filename .drawio).drawio -H 'Accept: application/pdf' http://localhost:5000/convert_file?crop=true --output ./out/$(basename $filename .drawio).pdf
+        curl -d @./diagrams/$(basename $filename .drawio).drawio -H 'Accept: application/pdf' http://127.0.0.1:5000/convert_file?crop=true --output ./out/$(basename $filename .drawio).pdf
     echo
 done
 
