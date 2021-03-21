3177281dec6dc3d03f27d21060cdaf6bae588ed2
wms2537 <swmeng@yes.my>
2021-03-21 15:22:06 +0800

    log

--- generate_pdfs.sh
+++ generate_pdfs.sh
@@ -3,7 +3,7 @@ docker build -t my-image .
 docker run -d -p 5000:5000 --name drawio --shm-size=1g my-image
 
 sleep 30
-
+docker logs drawio
 for filename in ./diagrams/*.drawio; do
     echo "### rendering $filename ..."
         curl -d @./diagrams/$(basename $filename .drawio).drawio -H 'Accept: application/pdf' http://localhost:5000/convert_file?crop=true --output ./out/$(basename $filename .drawio).pdf
