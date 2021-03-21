2749500f1aed851fa80f255801a774aeae31ca5f
wms2537 <swmeng@yes.my>
2021-03-21 16:53:39 +0800

    test

--- generate_pdfs.sh
+++ generate_pdfs.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 git checkout main
 docker build -t my-image .
-pwd
-ls
+
 docker run -d -v /var/jenkins_home/workspace/uec-physics_main/out:/work/out --name drawio --shm-size=1g my-image
 
 sleep 5
 docker exec drawio sh ./generate_pdf.sh
 docker logs drawio
 ls
-ls ./out
+ls $(pwd)/out
 
 docker kill drawio
 docker rm drawio
