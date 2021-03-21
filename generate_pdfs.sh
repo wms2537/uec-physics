0da9e9389536a6e0d982eecae9e620ee1a9c55f4
wms2537 <swmeng@yes.my>
2021-03-21 16:37:55 +0800

    test

--- generate_pdfs.sh
+++ generate_pdfs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 docker build -t my-image .
+pwd
+ls
 docker run -d -v $(pwd)/out:/work/out --name drawio --shm-size=1g my-image
 
 sleep 5
 docker exec drawio sh ./generate_pdf.sh
 docker logs drawio
-
+ls
+ls ./out
 
 docker kill drawio
 docker rm drawio
