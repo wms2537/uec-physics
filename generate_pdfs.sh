7d6122e637306b8efff54740a2cea12398490267
wms2537 <swmeng@yes.my>
2021-03-21 16:43:32 +0800

    path

--- generate_pdfs.sh
+++ generate_pdfs.sh
@@ -2,7 +2,7 @@
 docker build -t my-image .
 pwd
 ls
-docker run -d -v $(pwd)/out:/work/out --name drawio --shm-size=1g my-image
+docker run -d -v /var/jenkins_home/workspace/uec-physics_main/out:/work/out --name drawio --shm-size=1g my-image
 
 sleep 5
 docker exec drawio sh ./generate_pdf.sh
