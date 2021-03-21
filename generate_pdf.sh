41905efae59bd2b9d2938284a8b4517e591d4a22
wms2537 <swmeng@yes.my>
2021-03-21 19:00:36 +0800

    dir

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -1,3 +1,4 @@
+mkdir /work/out
 for filename in /work/diagrams/*.drawio; do
     echo "### rendering $filename ..."
         curl -d @/work/diagrams/$(basename $filename .drawio).drawio -H 'Accept: application/pdf' http://127.0.0.1:5000/convert_file?crop=true --output /work/out/$(basename $filename .drawio).pdf
